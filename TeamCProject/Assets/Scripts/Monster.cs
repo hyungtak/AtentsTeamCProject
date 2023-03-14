7613a43a0730a1e7c5f57e0404342a77e441b686
junho_Jeon <115610258+Pigon5@users.noreply.github.com>
2023-03-14 15:06:48 +0900

    몬스터 움직임
    180도 회전 중복됨
    [수정]

--- TeamCProject/Assets/Scripts/Monster.cs
+++ TeamCProject/Assets/Scripts/Monster.cs
@@ -1,3 +1,4 @@
+using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -12,8 +13,7 @@ public class Monster : MonoBehaviour
 
     public int monsterMove;
     //public int nextMove;
-    int left = -1;
-    
+    const int rotate = 180;
     
 
     Animator anim;
@@ -26,36 +26,41 @@ public class Monster : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
-        Invoke("backMove", 5);
+        Invoke("transMove", 7);
+        
 
     }
 
     private void FixedUpdate()
     {
         rigid.velocity = new Vector3(monsterMove, rigid.velocity.y, 0);
-        //Vector3 movement = new Vector3(monsterMove, 0, 0);
 
+        //Vector3 movement = new Vector3(monsterMove, 0, 0);
         //rigid.MovePosition(rigid.position + monsterMove * Time.fixedDeltaTime);
         //Vector3 front = new Vector3(Time.deltaTime*rigid.position.x + monsterMove, rigid.position.y, rigid.position.z);
-
-
-
-
-
+        
     }
 
-    void backMove()
+
+
+    void transMove()
     {
         //범위의 랜덤을 설정 후 생성
         monsterMove = Random.Range(-1, 2); //-1, 0, 1 왼쪽 멈춤 오른쪽
+        
 
+        Invoke("transMove", 7);
+        
+        anim.SetInteger("Walk", monsterMove);
 
-        Invoke("backMove", 5);
-        anim.SetInteger("walkSpeed", monsterMove);
-        //if(monsterMove != 0)
-        //{
-        //    transform.Rotate(0, 0, 180);
-        //}
+        
+        if (monsterMove != 0)
+        {   
+            transform.Rotate(0, rotate, 0);
+        }
     }
+
+    
+
 
 }
