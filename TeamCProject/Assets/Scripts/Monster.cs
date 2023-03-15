158fd53fef028fa686bb0dd9387ae95436133e97
junho_Jeon <115610258+Pigon5@users.noreply.github.com>
2023-03-15 15:20:42 +0900

    적 움직임 수정
    회전값 변경하기

--- TeamCProject/Assets/Scripts/Monster.cs
+++ TeamCProject/Assets/Scripts/Monster.cs
@@ -18,14 +18,14 @@ public class Monster : MonoBehaviour
     //몬스터 타입이 0,1 ture false
     private int MonsterType;
 
-    const int rotate = 180;
+    const int rotateSpeed = 90;
 
     //움직임 변환 변수
     private int move;
-
+    private int sotpStart;
     private Transform player;
 
-    bool find = false;
+    //bool find = false;
     GameObject traceTarget;
     Animator anim;
     Rigidbody rigid;
@@ -41,106 +41,109 @@ public class Monster : MonoBehaviour
         rigid = GetComponent<Rigidbody>();
 
         anim = GetComponent<Animator>();
-
-        //Invoke로
-        Invoke("transMovement", 3f);
-
     }
+    
 
+    
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-
-        //Vector3 dir = player.position - transform.position;
-        //dir.Normalize();
-        //GetComponent<Rigidbody>().velocity = dir * moveSpeed;
+        //FristMonsterMove();
+        StartCoroutine(transMovement());
+;
     }
 
     //FixedUpdate는 물리 시뮬레이션 갱신 주기에 맞춰서 호출된다. 
     private void FixedUpdate()
     {
 
-
-        MonsterMove();
-        //findPalyer();
-
-        //rigid.velocity = Time.deltaTime* moveSpeed * movement;
-        //Vector3 movement = new Vector3(monsterMove, 0, 0);
-        //rigid.MovePosition(rigid.position + monsterMove * Time.fixedDeltaTime);
-
-    }
-
-
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-           
-           anim.SetInteger("Walk", 1);
         
-        }
+        MonsterMove();
+
+        //if (find)
+        //{
+        //    //Vector3 player = traceTarget.transform.position;
+        //    Vector3 dir = player.position - transform.position;
+        //    dir.Normalize();
+        //    rigid.velocity = dir * moveSpeed;
+
+        //}
     }
 
 
-    //애니메이션 변화 
-    void transMovement()
-    {
-        //범위의 랜덤을 설정 후 생성
-        move = Random.Range(0, 3); //0, 1, 2 멈춤0 왼쪽1 오른쪽2
 
-
-        Invoke("transMovement", 3f);         //또 다시 호출해서 값을 받아온다
-
-        anim.SetInteger("Walk", move);
-
-        if (move != 0)
-        {
-            if (move == 1)
-            {
-                transform.Rotate(0, rotate, 0);
-            }
-        }
-    }
-    //좀 더 생각
-
-
-    //    //    //else(monsterMove == 0)
-    //    //    //{
-    //    //    //    transform.Rotate(0, rotate, rotate);
-    //    //    //}
-    //    //}
-    //}
-
-
-    //private void findPalyer()
+    //private void OnTriggerStay(Collider other)
     //{
-    //    if (find)
+    //    if (other.gameObject.tag == "Player")
     //    {
-    //        Vector3 direction = player.position - transform.position;
-    //        direction.Normalize();
-    //        GetComponent<Rigidbody>().velocity = direction * moveSpeed;
+    //        find = true;
+    //       anim.SetInteger("Walk", 1);
+
     //    }
     //}
 
-    private void MonsterMove()
+    IEnumerator transMovement()
     {
+        
 
-        if (find)
+        while (true)
         {
-            //Vector3 player = traceTarget.transform.position;
-            Vector3 dir = player.position - transform.position;
-                dir.Normalize();
-                GetComponent<Rigidbody>().velocity = dir * moveSpeed;
+            //범위의 랜덤을 설정 후 생성
+            move = Random.Range(0, 2);
+            
+            anim.SetInteger("Walk", move);
+            
+            
+            if(move == 1)
+            {
+                transform.localRotation = Quaternion.Euler(0, rotateSpeed, 0);
+            }
+            else if(move == 0) 
+            {
+                transform.localRotation = Quaternion.Euler(0, -rotateSpeed, 0);
+            }
+
+            yield return new WaitForSeconds(4f);
+            //if(move == )
 
         }
 
-        rigid.velocity = new Vector3(move, rigid.velocity.y, rigid.velocity.z);    //velocity.x,y,z는 0값이다.
-                                                                                   //move로 좌우측 계산
-
-
     }
 
+    //애니메이션 변화 
+    //void transMovement()
+    //{
+    //    //범위의 랜덤을 설정 후 생성
+    //    move = Random.Range(-1, 2); //-1, 0, 2 멈춤0 왼쪽-1 오른쪽1
+
+
+    //    Invoke("transMovement", 3f);         //또 다시 호출해서 값을 받아온다
+
+    //    anim.SetInteger("Walk", move);
+
+    //    //    //if (move != 0)
+    //    //    //{
+    //    //    //    if (move == 1)
+    //    //    //    {
+    //    //    //        transform.Rotate(0, rotate, 0);
+    //    //    //    }
+    //    //    //}
+    //}
+
+
+    private void MonsterMove()
+    {
+        
+        //rigid의 현재위치 + 초당 얼마 *속도_*
+        rigid.MovePosition(rigid.position + Time.fixedDeltaTime * move * transform.forward);    //velocity.x,y,z는 0값이다.
+                                                                                                            //move로 좌우측 계산
+    }
+
+
+    //private void FristMonsterMove()
+    //{
+    //    rigid.MovePosition(rigid.position + move * Time.fixedDeltaTime * moveSpeed * transform.forward);
+    //}
 
 
 
