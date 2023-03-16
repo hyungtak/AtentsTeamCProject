f66b11e6f767430831eb32d4b0f5ad8c133d5026
junho_Jeon <115610258+Pigon5@users.noreply.github.com>
2023-03-16 15:19:54 +0900

    애니메이션 수정완료
    공격 HP 추가

--- TeamCProject/Assets/Scripts/Monster.cs
+++ TeamCProject/Assets/Scripts/Monster.cs
@@ -7,40 +7,50 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    //추가 할 것
+    //몬스터 Hp int
+    public int monsterHp = 10;
+
+    //몬스터 Attack  or Damage 설정
+    public int monsterDamage = 1;
+
+    //수정
+
+    //플레이어 인식 시 플레이어쪽으로 회전.
 
 
-    //확인용
 
-    //몬스터 hp
-    //몬스터 attack
-
-    public float speedUp = 3f;
-
-    //몬스터 타입이 0,1 ture false
-    //private int MonsterType;
-
+    /// <summary>
+    /// 몬스터가 기본 회전값
+    /// </summary>
     const int transRotate = 180;
 
-    //움직임 변환 변수
+    /// <summary>
+    /// 애니메이션 변환값or속도값
+    /// </summary>
     private int move;
     
+    /// <summary>
+    /// 플레이어 위치 저장 할 변수
+    /// </summary>
+    private Transform player; 
 
-    private Transform player; //플레이어
-
+    /// <summary>
+    /// 플레이어 트리거 인식
+    /// </summary>
     bool find = false;
-
-    GameObject targetPlayer; 
+   
     Animator anim;
     Rigidbody rigid;
 
 
-
+    private Transform monsterTransform;
 
 
     private void Awake()
     {
 
-        //Component 가져오기
+        //필요한 Component 가져오기
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
   
@@ -50,8 +60,12 @@ public class Monster : MonoBehaviour
     
     private void Start()
     {
+        //Tag를 이용하여 Player의 transform을 player에 저장
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        //FristMonsterMove();
+       
+        transform.Rotate(0, 90, 0);
+      
+        //코루틴 실행
         StartCoroutine(transMovement());
 
     }
@@ -59,88 +73,104 @@ public class Monster : MonoBehaviour
     //FixedUpdate는 물리 시뮬레이션 갱신 주기에 맞춰서 호출된다. 
     private void FixedUpdate()
     {
+        //몬스터움직임 함수
         MonsterMove();
-        
     }
 
-    //태그 될 때
+    /// <summary>
+    /// 플레이어가 트리거에 접촉했다
+    /// </summary>
+    /// <param name="other">"Player"</param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            StopCoroutine(transMovement());
-
-            targetPlayer = other.gameObject;
-
+            StopAllCoroutines();
+            //monsterTransform.LookAt(player.transform.position);
+            find = true;
             anim.SetBool("Run", true);
         }
         
     }
 
-    //
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            
-            find = true;
-            StopCoroutine(transMovement());
-        }
-    }
+    //플레이어가 트리거 안에 지속적으로 인식
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        find = true;
+    //    }
+    //}
 
+
+    /// <summary>
+    /// 트리거 밖에 플레이어가 나갔을 때
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
-           find = false;
+        find = false;
 
         if (other.gameObject.tag == "Player")
-        {
+        {    
+           //Run애니메이션 >> Walk로
            anim.SetBool("Run", false);
-           StartCoroutine(transMovement());
-        
+           
+           //다시 자동 이동
+           StartCoroutine(transMovement());       
         }
         
     }
 
-    //몬스터 회전값 설정
+    /// <summary>
+    /// 자동 이동 시 몬스터 회전값 설정
+    /// </summary>
+    /// <returns></returns>
     IEnumerator transMovement()
-    {   
-
-        //시간을 받는다.
-         //float seconds = Random.Range(5f, 8f); 
-    
-            //범위의 랜덤을 설정 후 생성
-         move = Random.Range(0, 3);           
-         anim.SetInteger("Walk", move);
- 
-         if(move != 0)
-         {
-             transform.Rotate (0, transRotate, 0);  //좌우 회전
-         }
-          
-         yield return new WaitForSeconds(5f);
-
-
-        StartCoroutine(transMovement());        
+    {
         
+        while (true)
+        {
+            //move = 0 == Idle or move != 0 == Walk 실행
+            move = Random.Range(0, 3);
+            anim.SetInteger("Walk", move);
+
+            if (move != 0)
+            {
+                transform.Rotate(0, transRotate, 0);  //좌우 회전
+            }
+
+            //3초 마다
+            yield return new WaitForSeconds(3f);
+
+        }     
 
     }
 
 
-    //몬스터 움직임
+    /// <summary>
+    /// 몬스터 움직임
+    /// </summary>
     private void MonsterMove()
     {
-
+        //플레이어를 인식 했을 때 
         if(find)
         {
+            StopAllCoroutines();
+            move = 3;
+            //dir이 플레이어 방향 찾고 크기는 1 
             Vector3 dir = (player.position - transform.position).normalized;
-            rigid.MovePosition(transform.position + dir * Time.fixedDeltaTime);
-          
+
+            //몬스터 위치 + 속도 * DetaTime* 플레이 방향 
+            rigid.MovePosition(transform.position + move * Time.fixedDeltaTime * dir);
+
+            
 
         }
 
-        //rigid의 현재위치 + 초당 얼마 *속도*                          //move가 문제//move로 좌우측 계산
-     
-        rigid.MovePosition(rigid.position + Time.fixedDeltaTime *transform.forward);    //velocity.x,y,z는 0값이다.
+     //인식 안했을 때 행동                              
+     else
+        rigid.MovePosition(transform.position + Time.fixedDeltaTime * move *transform.forward);
         
     }
 
